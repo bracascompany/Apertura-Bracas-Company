9dce0ace4e1f2c1f361635ec52555a32d8e54d9b
José Antonio Bravo Castro <jbravo35@estudiantes.areandina.edu.co>
2026-09-03 16:35:34 -0500

    fix: remover vercel.json y unificar rutas dinamicas en Flask

--- api/index.py
+++ api/index.py
@@ -3,10 +3,11 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route("/api/hello", methods=["GET"])
-def hello():
+@app.route("/", defaults={"path": ""})
+@app.route("/<path:path>")
+def hello(path):
     return jsonify({
-        "mensaje_api": "¡Servidor Serverless de Vercel ejecutando Python nativo con exito!",
+        "mensaje_api": "¡Servidor Serverless de Vercel ejecutando Python con Flask con exito!",
         "proyecto": "bracascompany",
         "estado": "Operativo en la nube"
     })
