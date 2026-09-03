88b8b5ba2c97997be7e5a51b0cb08e63066ed6c9
José Antonio Bravo Castro <jbravo35@estudiantes.areandina.edu.co>
2026-09-03 16:13:33 -0500

    fix: simplificar estructura handler de Python para compatibilidad Serverless

--- api/hello.py
+++ api/hello.py
@@ -1,19 +1,18 @@
 
-from http.server import BaseHTTPRequestHandler
 import json
 
-class handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "application/json")
-        self.end_headers()
-        
-        response_data = {
-            "mensaje_api": "¡Servidor Serverless de Vercel ejecutando Python nativo con exito!",
-            "proyecto": "bracascompany",
-            "estado": "Operativo en la nube"
-        }
-        
-        self.wfile.write(json.dumps(response_data).encode("utf-8"))
-        return
+def handler(request):
+    response_data = {
+        "mensaje_api": "¡Servidor Serverless de Vercel ejecutando Python nativo con exito!",
+        "proyecto": "bracascompany",
+        "estado": "Operativo en la nube"
+    }
+    
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps(response_data)
+    }
 
