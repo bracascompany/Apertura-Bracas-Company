15090b723c9e67d40185ad1bdc54fbfb53d6713f
facebranddigital-Darkarmadeus <facebranddigital@gmail.com>
2026-09-05 23:17:18 -0500

    fix: final correction of environment import path in app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,7 @@ import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { provideStorage, getStorage } from '@angular/fire/storage';
 import { provideAuth, getAuth } from '@angular/fire/auth';
-import { environment } from '../environments/environment';
+import { environment } from "../environments/environment";
 
 import { routes } from './app.routes';
 
