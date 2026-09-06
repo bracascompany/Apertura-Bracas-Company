0cabe74ddd4f78048f6912c1c976108eec2cea98
José Antonio Bravo Castro <jbravo35@estudiantes.areandina.edu.co>
2026-09-05 19:55:29 -0500

    feat: ajuste al landing.html_Botón FSEGAT

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {production: false,
+  firebase: {
+    projectId: 'demo-project',
+    appId: 'demo-app',
+    storageBucket: 'demo-bucket',
+    apiKey: 'demo-key',
+    authDomain: 'demo-domain',
+    messagingSenderId: 'demo-sender'
+  }};
